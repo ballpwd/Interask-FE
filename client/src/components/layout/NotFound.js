b1265efa885b7bf0e680cd8387710e548c18a6e8
Kasipan A <kasipan.boss@gmail.com>
2020-10-26 20:25:57 +0700

    Add not found css
    - add not found css

--- client/src/components/layout/NotFound.js
+++ client/src/components/layout/NotFound.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import { Row } from "reactstrap";
 
 
 const NotFound = () => {
     return (
-		<div className='bg fullscreen'> 
-            <h1> Page not found</h1>
-            <div className='buttons'>
-                <Link to='/'>
-                    Go to Home
-                </Link>
+		<div className='bg fullscreen'>
+            <div className="content">
+                <h1>404</h1>
+                <h4>Oops! Page not found</h4>
+                <p>This page you were looking for doesn't exist.</p>
+                <div className='buttons'>
+                    <Link to='/'>
+                        <div className="nf-buttons">Go to Home</div>
+                    </Link>
+                </div>
             </div>
 		</div>
 	);
